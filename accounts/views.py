6fee6c2e3ac80a289f4c16b85c262954b81aec62
u-leslie <heavenmadeleslie@gmail.com>
2024-10-24 07:28:23 +0200

    fix: end fixes

--- accounts/views.py
+++ accounts/views.py
@@ -6,21 +6,19 @@ from django.contrib import messages
 from shipments.models import Shipment 
 from drivers.models import Driver
 
-# User Registration View
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()  # Save the new user
-            login(request, user)  # Log the user in after registration
-            return redirect('home')  # Redirect to home after registration
+            user = form.save() 
+            login(request, user)
+            return redirect('home')  
         else:
             messages.error(request, "Registration failed. Please try again.")
     else:
         form = UserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
 
-# User Login View
 def user_login(request):
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
@@ -30,7 +28,7 @@ def user_login(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('profile')  # Redirect to profile after login
+                return redirect('profile')  
             else:
                 messages.error(request, "Invalid username or password.")
         else:
@@ -39,19 +37,16 @@ def user_login(request):
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form': form})
 
-# User Logout View
 @login_required
 def user_logout(request):
     logout(request)
     return redirect('login')
 
-# Home View
 def home(request):
     return render(request, 'home.html')
 
-# User Profile View (Show user's shipments and drivers)
 @login_required
 def profile(request):
-    shipments = Shipment.objects.filter(user=request.user)  # Only show the shipments created by the logged-in user
-    drivers = Driver.objects.filter(user=request.user)  # Only show the drivers created by the logged-in user
+    shipments = Shipment.objects.filter(user=request.user) 
+    drivers = Driver.objects.filter(user=request.user) 
     return render(request, 'accounts/profile.html', {'shipments': shipments, 'drivers': drivers})
--- shipments/views.py
+++ shipments/views.py
@@ -1,49 +1,3 @@
-# from django.shortcuts import render, redirect, get_object_or_404
-# from django.urls import reverse
-# from .models import Shipment
-# from .forms import ShipmentForm
-
-# # List of all shipments
-# def shipment_list(request):
-#     shipments = Shipment.objects.all()
-#     return render(request, 'shipments/shipments_list.html', {'shipments': shipments})
-
-# # Shipment details
-# def shipment_detail(request, pk):
-#     shipment = get_object_or_404(Shipment, pk=pk)
-#     return render(request, 'shipments/shipment_detail.html', {'shipment': shipment})
-
-# def shipment_create(request):
-#     if request.method == 'POST':
-#         form = ShipmentForm(request.POST)
-#         if form.is_valid():
-#             form.save()  # Django automatically assigns the primary key
-#             return redirect('shipments_list')
-#     else:
-#         form = ShipmentForm()
-#     return render(request, 'shipments/shipment_form.html', {'form': form})
-
-# # Edit a shipment
-# def shipment_edit(request, pk):
-#     shipment = get_object_or_404(Shipment, pk=pk)
-#     if request.method == 'POST':
-#         form = ShipmentForm(request.POST, instance=shipment)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('shipments_list')
-#     else:
-#         form = ShipmentForm(instance=shipment)
-#     return render(request, 'shipments/shipment_edit.html', {'form': form})
-
-# # Delete a shipment
-# def shipment_delete(request, pk):
-#     shipment = get_object_or_404(Shipment, pk=pk)
-#     if request.method == 'POST':
-#         shipment.delete()
-#         return redirect('shipments_list')
-#     return render(request, 'shipments/shipment_delete.html', {'shipment': shipment})
-
-
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Shipment
@@ -52,13 +6,13 @@ from .forms import ShipmentForm
 # List of all shipments (login required)
 @login_required
 def shipment_list(request):
-    shipments = Shipment.objects.filter(user=request.user)  # Show only the shipments created by the logged-in user
+    shipments = Shipment.objects.filter(user=request.user) 
     return render(request, 'shipments/shipments_list.html', {'shipments': shipments})
 
 # Shipment details (login required)
 @login_required
 def shipment_detail(request, pk):
-    shipment = get_object_or_404(Shipment, pk=pk, user=request.user)  # Ensure the shipment belongs to the user
+    shipment = get_object_or_404(Shipment, pk=pk, user=request.user)  
     return render(request, 'shipments/shipment_detail.html', {'shipment': shipment})
 
 # Create a new shipment (login required)
